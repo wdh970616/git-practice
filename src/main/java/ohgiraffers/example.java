bc96c1800eda2c0adc010381edf4378de1c8920b
wdh970616 <wdh970616@naver.com>
2024-05-10 10:56:25 +0900

    first commit

---
+++ src/main/java/ohgiraffers/example.java
@@ -0,0 +1,10 @@
+package ohgiraffers;
+
+public class example {
+    public static void main(String[] args) {
+
+        System.out.print("Hello World!");
+        System.out.print("한글은 안 깨져요");
+
+    }
+}
