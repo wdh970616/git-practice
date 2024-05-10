dbc9e7796d1d2316693b9f338bcdea7ca4a0c6ed
wdh970616 <wdh970616@naver.com>
2024-05-10 11:10:02 +0900

    출력 문자 변경

--- src/main/java/ohgiraffers/example.java
+++ src/main/java/ohgiraffers/example.java
@@ -4,7 +4,7 @@ public class example {
     public static void main(String[] args) {
 
         System.out.print("Hello World!");
-        System.out.print("한글은 안 깨져요");
+        System.out.print("안녕하세요 2교시입니다");
 
     }
 }
